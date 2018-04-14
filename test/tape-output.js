50d05fbdbf78176793950c2dc2414d8395a8add1
samiralajmovic <alajmovic.samir@gmail.com>
2018-04-14 16:10:53 +0200

    test: run generate-tests

--- test/tape-output.js
+++ test/tape-output.js
@@ -3,6 +3,8 @@ const test = require('tape');
 test('Test message 1', t => {
   t.equal(1 === 1, true, 'optional message 0');
   t.equal(1 === 2, true, 'optional message 1');
+  t.equal(1 === 1, true, 'optional message 1');
+  t.deepEqual({ hej: 'san' }, { hej: 'an' }, 'optional message 1');
   t.end();
 });
 
